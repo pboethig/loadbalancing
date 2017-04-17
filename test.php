0d99b92ffed00b3881f2b2dbed0c13e79305ebc8
pboethig <pboethig@github.com>
2017-04-17 03:16:23 +0200

    add trst

--- test.php
+++ test.php
@@ -12,12 +12,13 @@ if(!isset($argv[2])) die("no url set: type: php test.php 1000 http://url.de/ 10"
 if(!isset($argv[3])) $argv[3] = 1;
 
 $repeats=$argv[1];
+$response = [];
 
 for($a=1;$a<$argv[3]+1;$a++)
 {
     $startScriptTime= microtime(TRUE);
 
-    echo PHP_EOL . '------------------------Test:'.$a.'-------------------------' . PHP_EOL;
+    echo PHP_EOL . '++++++++++++++++++++++++ Test: ' . $a .' +++++++++++++++++++++++++++' . PHP_EOL;
 
     for($i=1;$i<$repeats+1;$i++)
     {
@@ -26,33 +27,59 @@ for($a=1;$a<$argv[3]+1;$a++)
         $response[] = file_get_contents($argv[2]);
     }
 
+    printInvolvedHosts($response);
+
     $endScriptTime = microtime(TRUE);
 
     $totalScriptTime = $endScriptTime-$startScriptTime;
 
     $totalLoopTime += $totalScriptTime;
 
-    echo "\n\r".'<!-- Load time: '.$totalScriptTime.' seconds -->'. PHP_EOL;
-
-    echo PHP_EOL . 'requests per second:' . $argv[1] / $totalScriptTime  . PHP_EOL;
+    printSingleTestResults($totalScriptTime, $argv);
 }
 
-echo PHP_EOL . '########################## differernt Responses ###################################';
+printInvolvedHosts($response);
 
-foreach (array_unique($response) as $item)
+printAverages($totalRequests, $totalLoopTime);
+
+/**
+ * @param $totalScriptTime
+ * @param $argv
+ */
+function printSingleTestResults($totalScriptTime, $argv)
 {
-    echo PHP_EOL . $item;
+    echo "\n\r".'Load time: '.round($totalScriptTime,3).' seconds';
+
+    echo PHP_EOL . 'requests per second:' . round($argv[1] / $totalScriptTime,3)  . PHP_EOL;
 }
 
-echo PHP_EOL;
+/**
+ * @param $totalRequests
+ * @param $totalLoopTime
+ */
+function printAverages($totalRequests, $totalLoopTime)
+{
+    echo PHP_EOL;
 
-echo PHP_EOL . '########################## Average ###################################';
+    echo PHP_EOL . '########################## Average ###################################';
 
-echo PHP_EOL . 'total requests per second:' . round($totalRequests / $totalLoopTime,2);
+    echo PHP_EOL . 'Total requests per second:' . round($totalRequests / $totalLoopTime,2);
 
-echo PHP_EOL . 'total requests:' . $totalRequests;
+    echo PHP_EOL . 'Total requests:' . $totalRequests;
 
-echo PHP_EOL . 'total time:' . round($totalLoopTime,2) ;
+    echo PHP_EOL . 'Total time:' . round($totalLoopTime,2) ;
 
-echo PHP_EOL . '########################## Average ###################################' . PHP_EOL;
-?>
+    echo PHP_EOL . '########################## Average ###################################' . PHP_EOL;
+}
+/**
+ * @param $response
+ */
+function printInvolvedHosts($response)
+{
+    echo PHP_EOL . '------------------ Invoved hosts ------------------------';
+
+    foreach (array_unique($response) as $item)
+    {
+        echo PHP_EOL . $item;
+    }
+}
